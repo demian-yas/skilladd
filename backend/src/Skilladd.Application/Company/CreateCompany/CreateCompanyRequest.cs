780337eb06ab98d046711a1c406b525e53f373b0
demian <284421736+demian-yas@users.noreply.github.com>
2026-06-28 17:50:56 +0300

    refact datetime

--- backend/src/Skilladd.Application/Company/CreateCompany/CreateCompanyRequest.cs
+++ backend/src/Skilladd.Application/Company/CreateCompany/CreateCompanyRequest.cs
@@ -4,7 +4,7 @@ using Skilladd.Domain.Hiring.VO;
 namespace Skilladd.Application.Company.CreateCompany;
 
 public record CreateCompanyRequest(Guid ownerId ,string name, string slug, string? description, string? logoUrl, string? website, string? industry,
-    CompanySize? size, CreateCompanyAddressRequest? address ,CreateCompanyLocationRequest? location, DateTime createdAt);
+    CompanySize? size, CreateCompanyAddressRequest? address ,CreateCompanyLocationRequest? location);
 
 public record CreateCompanyAddressRequest(string? country, string? city, string? street);
 
